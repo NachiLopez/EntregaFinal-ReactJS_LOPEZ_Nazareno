9969ec923d9407af95c7f86e488394823de1406a
NachiLopez <nazarenolopez35@gmail.com>
2023-10-03 18:38:25 -0300

    Se modifico las rutas de imageUrl de rutas relativas a rutas absolutas y tratar de corregir el problema de visualicion de imagenes

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { initFirebase } from './firebase/firebaseconfig.js'
+import { initFirebase } from './firebase/firebaseConfig.js'
 
 initFirebase()
 ReactDOM.createRoot(document.getElementById('root')).render(
