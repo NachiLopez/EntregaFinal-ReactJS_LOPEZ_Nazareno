36ada6700020bca8e974984f8c1cbf846abfff1d
NachiLopez <nazarenolopez35@gmail.com>
2023-10-04 15:51:16 -0300

    Cambie la bd a la principal y cambie la url de las imagenes para que se puedan visualizar en la pag de vercel.

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,15 +1,15 @@
 import { addDoc, collection, doc, getFirestore, setDoc } from "firebase/firestore";
 
 const products = [
-    { name: 'Nike Huarache', category:'zapatillas', price:75999, desc:'Zapatillas de moda para hombre', imageUrl:'/public/NikeAirHuarache.webp', stock: 100 },
-    { name: 'Nike Air Force 1 LV8', category:'zapatillas', price:65999, desc:'Zapatillas de moda para niño', imageUrl:'/public/NikeAirForce1LV8.webp', stock: 100 },
-    { name: 'Nike Dunk Low SE', category:'zapatillas', price:79999, desc:'Zapatillas de moda para mujer', imageUrl:'/public/NikeDunkLowSE.webp', stock: 100 },
-    { name: 'BKRM Hoddie White', category:'hoddies', price:31900, desc:'Hoddie blanco oversize de la marca BKRM', imageUrl:'/public/bkrm-hoddie-white-men.jpeg', stock: 100 },
-    { name: 'BKRM Remera Black Woman', category:'remeras', price:11990, desc:'Remera negra para mujeres de la marca BKRM', imageUrl:'/public/bkrm-shirt-black-woman.jpeg', stock: 100 },
-    { name: 'BKRM Remera Blanca Woman', category:'remeras', price:11990, desc:'Remera blanca para mujeres de la marca BKRM', imageUrl:'/public/bkrm-shirt-white-woman.jpeg', stock: 100 },
-    { name: 'Hoddie gris Maria Jesus', category:'hoddies', price:29900, desc:'Hoddie gris oversize de Maria Jesus', imageUrl:'/public/mariajesus-gris-hoddie-men.jpeg', stock: 100 },
-    { name: 'Hoddie negro Maria Jesus', category:'hoddies', price:29900, desc:'Hoddie negro oversize de Maria Jesus', imageUrl:'/public/mariajesus-hoddie-black-men.jpeg', stock: 100 },
-    { name: 'Hoddie blanco Maria Jesus', category:'hoddies', price:29900, desc:'Hoddie blanco oversize de Maria Jesus', imageUrl:'/public/mariajesus-hoddie-white-men.jpeg', stock: 100 }
+    { name: 'Nike Huarache', category:'zapatillas', price:75999, desc:'Zapatillas de moda para hombre', imageUrl:'https://i.ibb.co/F0wDGtn/Nike-Air-Huarache.webp', stock: 100 },
+    { name: 'Nike Air Force 1 LV8', category:'zapatillas', price:65999, desc:'Zapatillas de moda para niño', imageUrl:'https://i.ibb.co/pJ3d1MN/Nike-Air-Force1-LV8.webp', stock: 100 },
+    { name: 'Nike Dunk Low SE', category:'zapatillas', price:79999, desc:'Zapatillas de moda para mujer', imageUrl:'https://i.ibb.co/0qjXkd5/Nike-Dunk-Low-SE.webp', stock: 100 },
+    { name: 'BKRM Hoddie White', category:'hoddies', price:31900, desc:'Hoddie blanco oversize de la marca BKRM', imageUrl:'https://i.ibb.co/mRrHfXQ/bkrm-hoddie-white-men.jpg', stock: 100 },
+    { name: 'BKRM Remera Black Woman', category:'remeras', price:11990, desc:'Remera negra para mujeres de la marca BKRM', imageUrl:'/https://i.ibb.co/4J0H38t/bkrm-shirt-black-woman.jpg', stock: 100 },
+    { name: 'BKRM Remera Blanca Woman', category:'remeras', price:11990, desc:'Remera blanca para mujeres de la marca BKRM', imageUrl:'https://i.ibb.co/hdjPg4T/bkrm-shirt-white-woman.jpg', stock: 100 },
+    { name: 'Hoddie gris Maria Jesus', category:'hoddies', price:29900, desc:'Hoddie gris oversize de Maria Jesus', imageUrl:'https://i.ibb.co/rMm5t4n/mariajesus-gris-hoddie-men.jpg', stock: 100 },
+    { name: 'Hoddie negro Maria Jesus', category:'hoddies', price:29900, desc:'Hoddie negro oversize de Maria Jesus', imageUrl:'https://i.ibb.co/KXWsr7T/mariajesus-hoddie-black-men.jpg', stock: 100 },
+    { name: 'Hoddie blanco Maria Jesus', category:'hoddies', price:29900, desc:'Hoddie blanco oversize de Maria Jesus', imageUrl:'https://i.ibb.co/G0YQZG8/mariajesus-hoddie-white-men.jpg', stock: 100 }
 ];
 
 export const fetchData = (pid) => {
